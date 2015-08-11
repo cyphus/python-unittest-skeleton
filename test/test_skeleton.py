ee4bbb7ff7619b3d84a779afac231ad03edfbd06
Terrance Kennedy <tkennedy@cars.com>
2015-08-11 21:46:16 +0000

    Make imports local to test

--- test/test_skeleton.py
+++ test/test_skeleton.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from skeleton.code import add
 
 class TestSkeleton(unittest.TestCase):
     def test_add(self):
+        from skeleton import add
         expected = 7
         actual = add(3, 4)
         self.assertEqual(expected, actual)
