22fe4ad78b7045d3d3d50c13accbf800db785c7f
Terrance Kennedy <tkennedy@cars.com>
2015-08-25 20:44:23 +0000

    Simplify import

--- test/test_skeleton.py
+++ test/test_skeleton.py
@@ -4,7 +4,7 @@ import unittest
 
 class TestSkeleton(unittest.TestCase):
     def test_add(self):
-        from skeleton import add
+        from skeleton.code import add
         expected = 7
         actual = add(3, 4)
         self.assertEqual(expected, actual)
